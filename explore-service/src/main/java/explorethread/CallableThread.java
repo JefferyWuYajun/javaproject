3f8d957b14d3bd7eac0cef75ac9ae66b1cff691f
wyj40124 <wyj40124@LY.com>
2018-02-09 17:31:10 +0800

    实现callable接口线程

--- explore-service/src/main/java/explorethread/CallableThread.java
+++ explore-service/src/main/java/explorethread/CallableThread.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author jefferywu
@@ -12,22 +14,31 @@ import java.util.concurrent.Callable;
  */
 public class CallableThread implements Callable<Object> {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private Logger         logger = LoggerFactory.getLogger(getClass());
 
-    private String taskNum;
+    private String         taskNum;
 
-    public CallableThread(String taskNum) {
+    private static Random  random = new Random();
+
+    private CountDownLatch countDownLatch;
+
+    public CallableThread(String taskNum, CountDownLatch countDownLatch) {
         this.taskNum = taskNum;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
     public Object call() throws Exception {
-        logger.info(">>>" + taskNum + "任务启动");
-        Date dateBegin = new Date();
-        Thread.sleep(Integer.parseInt(taskNum) * 1000);
-        Date dateEnd = new Date();
-        long excuteTime = dateEnd.getTime() - dateBegin.getTime();
-        logger.info("<<<" + taskNum + "任务终止,执行时间:" + excuteTime);
-        return taskNum + "任务返回运行结果，当前任务时间[" + excuteTime + "]毫秒";
+        try {
+            logger.info(">>>" + taskNum + "任务启动");
+            Date dateBegin = new Date();
+            Thread.sleep(random.nextInt(6) * 1000);
+            Date dateEnd = new Date();
+            long excuteTime = dateEnd.getTime() - dateBegin.getTime();
+            logger.info("<<<" + taskNum + "任务终止,执行时间:" + excuteTime);
+            return taskNum + "任务返回运行结果，当前任务时间[" + excuteTime + "]毫秒";
+        } finally {
+            countDownLatch.countDown();
+        }
     }
 }
--- explore-service/src/test/java/explorethread/CallableThreadTest.java
+++ explore-service/src/test/java/explorethread/CallableThreadTest.java
@@ -24,29 +24,47 @@ public class CallableThreadTest {
         ExecutorService pool = Executors.newFixedThreadPool(5);
         //创建多个有返回值的任务
         List<Future> futures = Lists.newArrayList();
-
+        //线程计数器
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        //计时器
         StopWatch stopWatch = new StopWatch();
 
-        stopWatch.start();
+        stopWatch.start("创建线程");
         for (int i = 0; i < 5; i++) {
-            Callable callable = new CallableThread(String.valueOf(i + 1));
+            CallableThread callable = new CallableThread(String.valueOf(i + 1), countDownLatch);
             futures.add(pool.submit(callable));
         }
         stopWatch.stop();
 
+
+        try {
+            stopWatch.start("主线程等待");
+            countDownLatch.await(3, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            logger.warn("countDownLatch异常", e);
+        } finally {
+            stopWatch.stop();
+        }
+
         //获取线程返回的结果
+        final int[] i = {0};
         futures.forEach((Future item) -> {
-            stopWatch.start();
+            i[0]++;
+            stopWatch.start("线程:" + i[0] + "取值");
             try {
-                logger.info(item.get(3, TimeUnit.SECONDS).toString());
+                logger.info(item.get(0, TimeUnit.SECONDS).toString());
             } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                logger.error(e.getMessage(), e);
+                logger.error("从子线程:" + i[0] + "获取值异常", e);
             }
             stopWatch.stop();
         });
 
+        //关闭线程池
         pool.shutdown();
 
         logger.info(stopWatch.prettyPrint());
+
+        //防止主线程结束,子线程还未跑完
+        Thread.sleep(10000);
     }
 }
