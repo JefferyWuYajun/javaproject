56e3e46cd7c09957da1ab5b32cbe032365573644
wyj40124 <wyj40124@LY.com>
2018-02-09 10:09:52 +0800

    userService

--- explore-service/src/main/java/service/impl/UserServiceImpl.java
+++ explore-service/src/main/java/service/impl/UserServiceImpl.java
@@ -12,5 +12,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public void test(String userName) throws Exception {
         System.out.println("test");
+        System.out.println("test2");
     }
 }
